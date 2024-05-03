db00e16360671b0bbecd3f4226a6f4d0474b1df6
naveengujjar29 <naveengujjar29@gmail.com>
2024-05-03 09:56:35 +0530

    Update startup-multi-process-players.sh

--- startup-multi-process-players.sh
+++ startup-multi-process-players.sh
@@ -2,8 +2,6 @@
 
 mvn clean package
 
-gnome-terminal -- java -jar receiver-socket/target/receiver-player-socket-1.0-SNAPSHOT.jar org.players.ReceiverPlayerSocket
+java -jar receiver-socket/target/receiver-player-socket-1.0-SNAPSHOT.jar org.players.ReceiverPlayerSocket &
 
-gnome-terminal -- java -jar initiator-socket/target/initiator-player-socket-1.0-SNAPSHOT.jar org.players.InitiatorPlayerSocket
-
-
+java -jar initiator-socket/target/initiator-player-socket-1.0-SNAPSHOT.jar org.players.InitiatorPlayerSocket
